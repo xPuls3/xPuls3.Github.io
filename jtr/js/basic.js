4b5df922da6cee09800e12d380642f6494d70af3
xPuls3 <Xer0.Puls3.XXII@gmail.com>
2019-12-15 21:59:57 -0500

    JTR Nation

---
+++ jtr/js/basic.js
@@ -0,0 +1,41 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    for (let i = 0; i < list.length; i++) {
+        addEntry(list[i].username, list[i].id, list[i].date);
+    }
+});
+
+function addEntry(username, id, date) {
+
+    let newListing = [];
+
+    newListing[0] = document.createElement('div');
+    newListing[0].className = "listing";
+
+    newListing[1] = document.createElement('div');
+    newListing[1].className = "header";
+    newListing[1].appendChild(document.createTextNode(username));
+    newListing[0].appendChild(newListing[1]);
+
+    newListing[2] = document.createElement('div');
+    newListing[2].className = "under";
+    newListing[0].appendChild(newListing[2]);
+
+    newListing[3] = document.createElement('ul');
+
+    newListing[4] = document.createElement('li');
+    newListing[4].appendChild(document.createTextNode("ID: " + id));
+    newListing[3].appendChild(newListing[4]);
+
+    newListing[5] = document.createElement('li');
+    newListing[5].appendChild(document.createTextNode("AKA: " + date));
+    newListing[3].appendChild(newListing[5]);
+
+    newListing[0].appendChild(newListing[3]);
+    document.getElementById("content").appendChild(newListing[0])
+
+}
+
+function redirect() {
+    window.location = window.location.origin;
+}
---
+++ jtr/js/list.js
@@ -0,0 +1,69 @@
+const list = [
+
+    {
+        username: "1JTR",
+        id: 334,
+		aka: "Puls3"
+    },
+
+    {
+        username: "2JTR",
+        id: 5028,
+		aka "2JTR"
+    },
+	
+    {
+        username: "3JTR",
+        id: 779,
+		aka: "InspectorGadget"
+    },
+
+    {
+        username: "4JTR",
+        id: 287,
+		aka "Yuno"
+    },
+	
+    {
+        username: "5JTR",
+        id: 5678,
+		aka "Starcrater"
+    },
+	
+    {
+        username: "6JTR",
+        id: 5831,
+		aka "GodMode"
+    },
+	
+    {
+        username: "7JTR",
+        id: 5430,
+		aka "Supernose"
+    },
+	
+    {
+        username: "8JTR",
+        id: 2256,
+		aka "Damien"
+    },
+	
+    {
+        username: "9JTR",
+        id: 289,
+		aka "Nuts"
+    },
+	
+    {
+        username: "10JTR",
+        id: 4081,
+		aka "Chameleon"
+    },
+	
+    {
+        username: "69JTR",
+        id: 60,
+		aka "Silent"
+    },
+
+];
